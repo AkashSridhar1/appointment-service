359982f2229b029b77f8dc23881953ec84957455
AkashSridhar1 <124769656+AkashSridhar1@users.noreply.github.com>
2025-06-18 01:28:58 +0530

    Update s3_dynamodb.tf

--- terraform/global/s3_dynamodb.tf
+++ terraform/global/s3_dynamodb.tf
@@ -1,15 +1,15 @@
-resource "aws_s3_bucket" "tf_state" {
+resource "aws_s3_bucket" "tf_state1" {
   bucket = "my-tf-state-bucket-test-20250617"
 }
 resource "aws_s3_bucket_versioning" "tf_state_versioning" {
-  bucket = aws_s3_bucket.tf_state.id
+  bucket = aws_s3_bucket.tf_state1.id
 
   versioning_configuration {
     status = "Enabled"
   }
 }
 
-resource "aws_dynamodb_table" "tf_lock" {
+resource "aws_dynamodb_table" "tf_lock1" {
   name         = "my-tf-lock-table1--20250617"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
