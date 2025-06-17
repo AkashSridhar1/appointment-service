9001ba115257685aeee78e6e26caea8a3f468770
AkashSridhar1 <124769656+AkashSridhar1@users.noreply.github.com>
2025-06-18 01:32:46 +0530

    Update backend.tf

--- terraform/envs/prod/backend.tf
+++ terraform/envs/prod/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
     bucket         = "my-tf-state-bucket-test-20250617"
-    key            = "dev/terraform.tfstate"
+    key            = "prod/terraform.tfstate"
     region         = "us-west-2"
-    dynamodb_table = "my-tf-lock-table1"
+    dynamodb_table = "my-tf-lock-table1-20250617"
     encrypt        = true
   }
 }
