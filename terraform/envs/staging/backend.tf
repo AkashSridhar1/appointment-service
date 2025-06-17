04d277e51f6cd25a0ba26d21a1c397a2287dc752
AkashSridhar1 <124769656+AkashSridhar1@users.noreply.github.com>
2025-06-18 01:30:06 +0530

    Update backend.tf

--- terraform/envs/staging/backend.tf
+++ terraform/envs/staging/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-tf-state-bucket-test"
+    bucket         = "my-tf-state-bucket-test-20250617"
     key            = "dev/terraform.tfstate"
     region         = "us-west-2"
     dynamodb_table = "my-tf-lock-table1"
